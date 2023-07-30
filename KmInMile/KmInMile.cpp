1d1c31dffd90eb7123e3c21ffc32b45126e8f82a
Gridin_Andrei <andreigridin918@gmail.com>
2023-07-30 21:12:54 +0500

    Добавьте файлы проекта.

---
+++ KmInMile/KmInMile.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+double KmInMile(int k) {
+    double mile;
+    const double kmInMile = 1.609344;
+    mile = k / kmInMile;
+    return mile;
+
+}
+
+int main()
+{
+    system("chcp 1251 > nul");
+    int km;
+    cout << "Введите растояние в километрах: ";
+    cin >> km;
+    cout << "В " << km << " километрах " << KmInMile(km) << " миль." << endl;
+    system("pause > nul");
+    return 0;
+}
